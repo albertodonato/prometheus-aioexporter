75e40943d3289d6faddb9c69ae9742a3804e9ff9
Alberto Donato <alberto.donato@gmail.com>
2017-02-26 18:34:09 +0100

    Remove exports.

--- prometheus_aioexporter/__init__.py
+++ prometheus_aioexporter/__init__.py
@@ -1,15 +1,3 @@
 '''Asyncio library for creating Prometheus exporters.'''
 
-from .metric import (
-    MetricConfig,
-    InvalidMetricType,
-    create_metrics)
-from .web import PrometheusExporterApplication
-from .script import PrometheusExporterScript
-
-
 __version__ = '0.1.0'
-
-__all__ = [
-    'MetricConfig', 'InvalidMetricType', 'create_metrics',
-    'PrometheusExporterApplication', 'PrometheusExporterScript']
