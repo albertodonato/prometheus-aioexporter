3a653ad99de996d4a8ed5c916cc686f2fb6089da
Alberto Donato <alberto.donato@gmail.com>
2017-02-26 10:56:57 +0100

    Export top-level.

--- prometheus_aioexporter/__init__.py
+++ prometheus_aioexporter/__init__.py
@@ -1,3 +1,14 @@
 '''Asyncio library for creating Prometheus exporters.'''
 
+from .metric import (
+    MetricConfig,
+    InvalidMetricType,
+    create_metrics)
+from .web import PrometheusExporterApplication
+
+
 __version__ = '0.1.0'
+
+__all__ = [
+    'MetricConfig', 'InvalidMetricType', 'create_metrics',
+    'PrometheusExporterApplication']
